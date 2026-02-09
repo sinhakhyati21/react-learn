19691e7b17b6c235a15e5626f8d0e1a3089c1480
Khyati Sinha <sinhakhyati21@gmail.com>
2026-02-09 10:13:24 +0530

    bug fixed

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -17,7 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,6 +27,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -33,4 +35,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
